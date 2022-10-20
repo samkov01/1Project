abdbbe84428e85168e90a73e80099a0e4968db17
samkov01 <ssmkov01@gmail.com>
2022-10-21 02:19:58 +0300

    project1

---
+++ GenshinTierList-master/GenshinTierList/Models/TierList.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebApp.Models
+{
+    public class TierList
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public Atribute Atribute { get; set; }
+
+        [Required]
+        public string Tier { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        [Display(Name = "Wiki link")]
+        public string WikiUrl { get; set; }
+
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Character")]
+        public string ImageUrl { get; set; }
+    }
+}
